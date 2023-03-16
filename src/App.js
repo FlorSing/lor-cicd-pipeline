bd2da95b96470e189262f79a864ef2292d2b3f2c
FlorSing <fpsingcak@gmail.com>
2023-03-16 19:59:58 +0000

    upload files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CharacterGallery from './components/CharacterGallery';
+
+function App() {
+  return (
+      <div data-test="component-app">
+      <CharacterGallery />
+      </div>
+      );
+  
+}
+
+export default App;
