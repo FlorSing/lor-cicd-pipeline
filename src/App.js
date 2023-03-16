ea2993e13b1b618f7a114a369901ccc160937e17
FlorSing <fpsingcak@gmail.com>
2023-03-16 20:52:20 +0000

    try deploy again

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CharacterGallery from './components/CharacterGallery';
 function App() {
   return (
       <div data-test="component-app">
+        <h1>LORD OF THE RINGS</h1>
       <CharacterGallery />
       </div>
       );
